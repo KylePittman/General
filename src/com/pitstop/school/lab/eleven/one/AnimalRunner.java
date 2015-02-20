0f0576ee2fe5d5da707be083f99b4a09c026a897
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-02-20 11:15:21 -0500

    Lab 11.1
    Fixed 1.2

--- src/com/pitstop/school/lab/eleven/one/AnimalRunner.java
+++ src/com/pitstop/school/lab/eleven/one/AnimalRunner.java
@@ -12,11 +12,19 @@ public class AnimalRunner
     public static void main(String[] args)
 
     {
-        Dog d1 = new Dog("Fred");
+        /*Dog d1 = new Dog("Fred");
         d1.speak();
         Object obj = new Dog("Connie");
 
         Dog d2 = (Dog) obj;
+        d2.speak();*/
+
+        Dog d1 = new Dog("Fred");
+        d1.speak();
+        Object obj = new Dog("Connie");
+        Dog d2 = (Dog) obj;
         d2.speak();
+
+
     }
 }
