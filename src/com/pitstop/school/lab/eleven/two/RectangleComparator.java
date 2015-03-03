8cb358a56212cfa72309cd33d80395c2ef6ee1ad
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-03-02 19:16:27 -0500

    Lab 11.2
    Incomplete

---
+++ src/com/pitstop/school/lab/eleven/two/RectangleComparator.java
@@ -0,0 +1,20 @@
+package com.pitstop.school.lab.eleven.two;//Kyle Pittman 3/2/2015.
+
+import java.util.Comparator;
+import java.awt.Rectangle;
+public class RectangleComparator implements Comparator<Rectangle>
+{
+    /**
+     Compares two Rectangle objects.
+     @param r1 the first rectangle
+     @param r2 the second rectangle
+     @return 1 if the area of the first rectangle is larger than the
+     area of
+     rectangle is
+     two
+     */
+    public int compare(Rectangle r1, Rectangle r2)
+    {
+        
+    }
+}
