98e758971bb0840332d51281c4650480d4bef5b5
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-03-12 11:07:02 -0400

    Cous's Work

---
+++ src/com/cousCous/school/lab/Eleven/two/RectangleComparator.java
@@ -0,0 +1,34 @@
+//Marcus Silcox
+package com.cousCous.school.lab.Eleven.two;
+import java.util.Comparator;
+
+import java.awt.Rectangle;
+
+public class RectangleComparator implements Comparator<Rectangle>
+
+{
+
+    /**
+     Compares two Rectangle objects.
+     @param r1 the first rectangle
+     @param r2 the second rectangle
+     @return 1 if the area of the first rectangle is larger than the
+     area of
+     rectangle is
+     two
+     */
+
+    public int compare(Rectangle r1, Rectangle r2)
+
+    {
+
+        return 0;
+    }
+
+}
+
+        /*the second rectangle, ­1 if the area of the first
+
+        smaller than the area of the second rectangle or 0 if the
+
+        rectangles have the same area*/
