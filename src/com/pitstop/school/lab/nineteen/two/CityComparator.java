b88c540ecd357b4c2670e8492aaa33f381178a82
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-05-22 10:56:05 -0400

    Lab 19.2 revision
    City Comparator was supposed to compare by state not city.

--- src/com/pitstop/school/lab/nineteen/two/CityComparator.java
+++ src/com/pitstop/school/lab/nineteen/two/CityComparator.java
@@ -11,16 +11,16 @@ public class CityComparator implements Comparator<City>
         char thisChar;
         char thatChar;
 
-        for (int i = 0; i < a.getName().length() && i < b.getName().length(); i++) {
-            thisChar = a.getName().charAt(i);
-            thatChar = b.getName().charAt(i);
+        for (int i = 0; i < a.getState().length() && i < b.getState().length(); i++) {
+            thisChar = a.getState().charAt(i);
+            thatChar = b.getState().charAt(i);
             if(thisChar < thatChar) return -1;
             if (thatChar < thisChar) return 1;
         }
 
-        for (int i = 0; i < a.getState().length() && i < b.getState().length(); i++) {
-            thisChar = a.getState().charAt(i);
-            thatChar = b.getState().charAt(i);
+        for (int i = 0; i < a.getName().length() && i < b.getName().length(); i++) {
+            thisChar = a.getName().charAt(i);
+            thatChar = b.getName().charAt(i);
             if(thisChar < thatChar) return -1;
             if (thatChar < thisChar) return 1;
         }
